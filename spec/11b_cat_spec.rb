7fb75ed17e7dfb53f4be669357d3c92330c6e481
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-01 14:46:14 +0100

    feat: finish assignments in spec/11b

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:frodo) { described_class.new("Frodo", nil, "orange") }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when the method name is the same in multiple classes' do
+    include_examples "shared method name"
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(frodo).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(frodo).to be_hiding
   end
 end

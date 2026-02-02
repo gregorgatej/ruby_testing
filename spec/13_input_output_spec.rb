c68c84e252b769ed2b210e0dca77881be6f09903
Gregor Gatej <gregor.gatej@gmail.com>
2026-02-02 13:16:24 +0100

    feat: finish assignments in spec/13

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -81,7 +81,7 @@ describe NumberGame do
     # Create a new instance of NumberGame and write a test for when the @guess
     # does not equal @solution.
     context 'when user guess is not correct' do
-      subject(:game_continues) { described_class.new(8, "1") }
+      subject(:game_continues) { described_class.new(8, '1') }
       # remove the 'x' before running this test
       it 'is not game over' do
         expect(game_continues).not_to be_game_over
@@ -98,7 +98,7 @@ describe NumberGame do
 
   describe '#verify_input' do
     subject(:game_check) { described_class.new }
-    # Note: #verify_input will only return a value if it matches /^[0-9]$/
+    # NOTE: #verify_input will only return a value if it matches /^[0-9]$/
 
     context 'when given a valid input as argument' do
       it 'returns valid input' do
@@ -113,7 +113,7 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when given invalid input as argument' do
       it 'returns nil' do
-        user_input = "10"
+        user_input = '10'
         verified_input = game_check.verify_input(user_input)
         expect(verified_input).to be_nil
       end
@@ -143,6 +143,14 @@ describe NumberGame do
         # To use a message expectation, move 'Assert' before 'Act'.
         expect(game_loop).not_to receive(:puts).with('Input error!')
         game_loop.player_turn
+      end
+
+      it 'stops loop and does not display error message (using spies)' do
+        valid_input = '4'
+        allow(game_loop).to receive(:player_input).and_return(valid_input)
+        spy = allow(game_loop).to receive(:puts)
+        game_loop.player_turn
+        expect(game_loop).not_to have_received(:puts).with('Input error!')
       end
     end
 
@@ -176,9 +184,15 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        first_invalid_input = 'm'
+        second_invalid_input = '99'
+        valid_input = '4'
+        allow(game_loop).to receive(:player_input).and_return(first_invalid_input, second_invalid_input, valid_input)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
@@ -210,9 +224,11 @@ describe NumberGame do
     # @guess, and @count
     context 'when count is 2-3' do
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      subject(:game_two) { described_class.new(6, '6', 3) }
+
+      it 'outputs correct phrase' do
         congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
-        expect { game.final_message }.to output(congrats_phrase).to_stdout
+        expect { game_two.final_message }.to output(congrats_phrase).to_stdout
       end
     end
 
@@ -220,8 +236,11 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when count is 4 and over' do
+      subject(:game_four) { described_class.new(1, '1', 4) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        hard_recognition_phrase = "That was hard. It took you 4 guesses!\n"
+        expect { game_four.final_message }.to output(hard_recognition_phrase).to_stdout
       end
     end
   end
